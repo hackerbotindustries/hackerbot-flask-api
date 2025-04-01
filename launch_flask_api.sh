a9ead419452519072b5912d0474a9598a98dfae5
Allen Chien <allen71090@gmail.com>
2025-04-01 17:58:25 -0600

    add file headers to launch scripts and test files

--- launch_flask_api.sh
+++ launch_flask_api.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+################################################################################
+# Copyright (c) 2025 Hackerbot Industries LLC
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Created By: Allen Chien
+# Created:    April 2025
+# Updated:    2025.04.01
+#
+# This script launches the Hackerbot Flask API process and occupies the serial port.
+#
+# Special thanks to the following for their code contributions to this codebase:
+# Allen Chien - https://github.com/AllenChienXXX
+################################################################################
+
+
 set -o pipefail
 
 BASE_DIR="$(pwd)"
