7c30f33f87440236b41b0799bf8524359db31cda
Allen Chien <allen71090@gmail.com>
2025-04-01 18:01:04 -0600

    add headers to the source files in the app folder

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,20 @@
+################################################################################
+# Copyright (c) 2025 Hackerbot Industries LLC
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Created By: Allen Chien
+# Created:    April 2025
+# Updated:    2025.04.01
+#
+# This script registers the routes for the Flask application.
+#
+# Special thanks to the following for their code contributions to this codebase:
+# Allen Chien - https://github.com/AllenChienXXX
+################################################################################
+
+
 from app.routes import status
 from app.routes import mapping
 from app.routes import action
--- app/run.py
+++ app/run.py
@@ -1,3 +1,20 @@
+################################################################################
+# Copyright (c) 2025 Hackerbot Industries LLC
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Created By: Allen Chien
+# Created:    April 2025
+# Updated:    2025.04.01
+#
+# This script runs the Flask application.
+#
+# Special thanks to the following for their code contributions to this codebase:
+# Allen Chien - https://github.com/AllenChienXXX
+################################################################################
+
+
 from app import create_app
 
 app = create_app()
