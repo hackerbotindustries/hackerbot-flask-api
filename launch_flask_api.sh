aaa23baeb044f90aab7a5dc71fbc772a1e6b0e0c
AllenChienXXX <allen71090@gmail.com>
2025-04-26 14:30:44 -0600

    include stop in launch scripts

--- launch_flask_api.sh
+++ launch_flask_api.sh
@@ -7,7 +7,7 @@
 #
 # Created By: Allen Chien
 # Created:    April 2025
-# Updated:    2025.04.01
+# Updated:    2025.04.26
 #
 # This script launches the Hackerbot Flask API process and occupies the serial port.
 #
@@ -57,6 +57,17 @@ function echo_failure {
 echo "---------------------------------------------"
 echo "STARTING HACKERBOT FLASK API"
 echo "---------------------------------------------"
+
+# Stop existing processes first
+if command -v stop-flask-api >/dev/null 2>&1; then
+    echo "Cleaning up existing process"
+    if ! stop-flask-api >> "$logfile_backend" 2>&1; then
+        echo "Failed to execute cleanup..."
+    fi
+else
+    echo "stop-flask-api command not found. Skipping cleanup."
+fi
+
 
 # Start Flask Backend
 # FLASK_APP=app.py FLASK_ENV=development flask run --host=0.0.0.0 --port=$FLASK_PORT --no-debugger --no-reload >> "$logfile_backend" 2>&1 &
